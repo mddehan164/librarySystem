c12f59725f967b13e0720e353092a166511b8c1c
MD Dehan <mddehan164@gmail.com>
2025-01-05 19:18:51 +0600

    fifth changes

--- script.js
+++ script.js
@@ -81,6 +81,12 @@ departmentBtn.addEventListener('click', function(){
     homePage.style.display = 'none';
     loginBox.style.display = 'none';
     sideBar.style.display = 'none';
+    facParent.style.display = 'flex';
+    depDialog.style.display = 'none';
+    deparBoxArr.forEach((e)=>{
+        e.style.display = 'none';
+        document.getElementById('choose').innerText = 'Faculty';
+    })
 
 })
 
@@ -90,8 +96,123 @@ departmentBtn.addEventListener('click', function(){
     sideBar.style.display = 'none';
     menuBtn.style.display = 'initial';
     departments.style.display = 'block';
+    facParent.style.display = 'flex';
+    deparBoxArr.forEach((e)=>{
+        e.style.display = 'none';
+        document.getElementById('choose').innerText = 'Faculty';
+    })
 })
 
+
+
+//faculty boxes handle
+
+let facParent = document.querySelector('.dept-faculty-parent');
+let facultyBox = document.getElementsByClassName('faculty-box');
+let departBoxes = document.getElementsByClassName('depart-box');
+let departParent = document.querySelector('.depart-parent');
+let depDialog = document.querySelector('.dept-dialog-parent');
+let facultyArr = Array.from(facultyBox);
+let deparBoxArr = Array.from(departBoxes);
+
+    facultyArr.forEach((e)=>{
+        e.addEventListener('click', ()=>{
+            document.getElementById('choose').innerText = 'Department';
+            depDialog.style.display = 'none';
+            semesterBox.style.display = 'none';
+        })
+    })
+    facultyArr[0].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[0].style.display = 'block';
+    })
+    facultyArr[1].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[1].style.display = 'block';
+    })
+    facultyArr[2].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[2].style.display = 'block';
+    })
+    facultyArr[3].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[3].style.display = 'block';
+    })
+    facultyArr[4].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[4].style.display = 'block';
+    })
+    facultyArr[5].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[5].style.display = 'block';
+    })
+    facultyArr[6].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[6].style.display = 'block';
+    })
+    facultyArr[7].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[7].style.display = 'block';
+    })
+    facultyArr[7].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[7].style.display = 'block';
+    })
+    facultyArr[8].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[8].style.display = 'block';
+    })
+    facultyArr[9].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[9].style.display = 'block';
+    })
+    facultyArr[10].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[10].style.display = 'block';
+    })
+    facultyArr[11].addEventListener('click', ()=>{
+        facParent.style.display = 'none';
+        departParent.style.display = 'flex';
+        deparBoxArr[11].style.display = 'block';
+    })
+
+    deparBoxArr.forEach(e=>{
+        e.addEventListener('click', ()=>{
+        depDialog.style.display = 'flex';
+        document.getElementById('minimize').addEventListener('click',()=>{
+            depDialog.style.display = 'none';
+        })
+        })
+    })
+
+
+    //handle year
+    let semesterBox = document.querySelector('.semester-box');
+    let years = document.getElementsByClassName('dept-years');
+    let yearsArr = Array.from(years);
+    
+    yearsArr.forEach((e)=>{
+        e.addEventListener('click',()=>{
+            semesterBox.style.display = 'flex';
+            document.getElementById('minimize').style.display = 'none';
+            document.getElementById('minimize2').addEventListener('click',()=>{
+                semesterBox.style.display = 'none';
+                document.getElementById('minimize').style.display = 'initial';
+            })
+        })
+    })
 //handle department boxes
 
 // let depChild = document.getElementsByClassName('dep-child');
